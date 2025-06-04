44f632c38f09f68e4f11afb3915a632f6310a79c
Rupert Swarbrick <rswarbrick@lowrisc.org>
2025-06-04 17:56:01 +0100

    [tlul_adapter_reg_tb,fpv] Test more parameter values
    This is just wiring things up so that there are eight instances in the
    testbench with different parameter values. These instances can share
    inputs driven by the tool, but we have to give them separate output
    ports.
    
    I've set CmdIntgCheck by default (because OpenTitan currently always
    sets the parameter), but allow the access latency to vary and also
    whether integrity values get generated.
    
    Signed-off-by: Rupert Swarbrick <rswarbrick@lowrisc.org>

--- hw/ip/tlul/fpv/tb/tlul_adapter_reg_tb.sv
+++ hw/ip/tlul/fpv/tb/tlul_adapter_reg_tb.sv
@@ -8,51 +8,69 @@ module tlul_adapter_reg_tb
   import tlul_pkg::*;
   import prim_mubi_pkg::mubi4_t;
 #(
-  parameter  bit CmdIntgCheck      = 0,  // 1: Enable command integrity check
-  parameter  bit EnableRspIntgGen  = 0,  // 1: Generate response integrity
-  parameter  bit EnableDataIntgGen = 0,  // 1: Generate response data integrity
+  parameter  bit CmdIntgCheck      = 1,  // 1: Enable command integrity check
   parameter  int RegAw             = 8,  // Width of register address
   parameter  int RegDw             = 32, // Shall be matched with TL_DW
-  parameter  int AccessLatency     = 0,  // 0: same cycle, 1: next cycle
   localparam int RegBw             = RegDw/8
 ) (
   input logic              clk_i,
   input logic              rst_ni,
   input  tl_h2d_t          tl_i,
-  output tl_d2h_t          tl_o,
   input  mubi4_t           en_ifetch_i,
-  output logic             intg_error_o,
-  output logic             re_o,
-  output logic             we_o,
-  output logic [RegAw-1:0] addr_o,
-  output logic [RegDw-1:0] wdata_o,
-  output logic [RegBw-1:0] be_o,
   input logic              busy_i,
   input logic  [RegDw-1:0] rdata_i,
   input logic              error_i
 );
 
-  tlul_adapter_reg #(
-    .CmdIntgCheck(CmdIntgCheck),
-    .EnableRspIntgGen(EnableRspIntgGen),
-    .EnableDataIntgGen(EnableDataIntgGen),
-    .RegAw(RegAw),
-    .RegDw(RegDw),
-    .AccessLatency(AccessLatency)
-  ) dut (.*);
+  for (genvar latency = 0; latency < 2; latency++) begin : gen_latency
+    for (genvar intg_gen = 0; intg_gen < 4; intg_gen++) begin : gen_intg_gen
+      tl_d2h_t          tl_out;
+      logic             intg_error_out;
+      logic             re_out;
+      logic             we_out;
+      logic [RegAw-1:0] addr_out;
+      logic [RegDw-1:0] wdata_out;
+      logic [RegBw-1:0] be_out;
 
-  // Add properties that assume tl_i is being driven in a valid way by the outside world and assert
-  // that the tl_o signal we produce behaves correctly.
-  tlul_assert #(.EndpointType("Device")) i_tlul_assert(.clk_i, .rst_ni, .h2d (tl_i), .d2h (tl_o));
+      tlul_adapter_reg #(
+        .CmdIntgCheck(CmdIntgCheck),
+        .EnableRspIntgGen(intg_gen[0]),
+        .EnableDataIntgGen(intg_gen[1]),
+        .RegAw(RegAw),
+        .RegDw(RegDw),
+        .AccessLatency(latency)
+      ) dut (
+        .clk_i,
+        .rst_ni,
+        .tl_i,
+        .tl_o         (tl_out),
+        .en_ifetch_i,
+        .intg_error_o (intg_error_out),
+        .re_o         (re_out),
+        .we_o         (we_out),
+        .addr_o       (addr_out),
+        .wdata_o      (wdata_out),
+        .be_o         (be_out),
+        .busy_i,
+        .rdata_i,
+        .error_i
+      );
 
-  // Assertions about the exact dut behaviour.
-  tlul_adapter_reg_assert_fpv #(
-    .CmdIntgCheck(CmdIntgCheck),
-    .EnableRspIntgGen(EnableRspIntgGen),
-    .EnableDataIntgGen(EnableDataIntgGen),
-    .RegAw(RegAw),
-    .RegDw(RegDw),
-    .AccessLatency(AccessLatency)
-  ) i_assert_fpv (.clk_i, .rst_ni);
+      // Add properties that assume tl_i is being driven in a valid way by the outside world and assert
+      // that the tl_o signal we produce behaves correctly.
+      tlul_assert #(.EndpointType("Device")) i_tlul_assert(.clk_i, .rst_ni,
+                                                           .h2d (tl_i), .d2h (tl_out));
+
+      // Assertions about the exact dut behaviour.
+      tlul_adapter_reg_assert_fpv #(
+        .CmdIntgCheck(1),
+        .EnableRspIntgGen(intg_gen[0]),
+        .EnableDataIntgGen(intg_gen[1]),
+        .RegAw(RegAw),
+        .RegDw(RegDw),
+        .AccessLatency(latency)
+      ) i_assert_fpv (.clk_i, .rst_ni);
+    end
+  end
 
 endmodule
