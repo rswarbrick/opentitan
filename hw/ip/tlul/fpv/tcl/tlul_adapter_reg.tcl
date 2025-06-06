cca7597facc5434d8495381872a834c58819b787
Rupert Swarbrick <rswarbrick@lowrisc.org>
2025-06-06 22:06:49 +0100

    [tlul_adapter_reg,fpv] Add after_load script for tlul_adapter_reg
    Signed-off-by: Rupert Swarbrick <rswarbrick@lowrisc.org>

---
+++ hw/ip/tlul/fpv/tcl/tlul_adapter_reg.tcl
@@ -0,0 +1,5 @@
+# Copyright lowRISC contributors (OpenTitan project).
+# Licensed under the Apache License, Version 2.0, see LICENSE for details.
+# SPDX-License-Identifier: Apache-2.0
+
+# TCL script used as after_load for verification of tlul_adapter_reg.tcl
