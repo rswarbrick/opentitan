3b804d1bee4b570161265a2c78bfca6f90e9e1c6
Rupert Swarbrick <rswarbrick@lowrisc.org>
2025-06-06 09:46:10 +0100

    [tlul_adapter_reg_tb,fpv] Properties for the A,D chan <-> reg iface
    The assertions in the first section check that a TL message to read a
    particular address will be correctly translated into a read
    request (using re_o and addr_o) and that that re_o only happens if we
    see a valid Get message.
    
    This is quite fiddly to define, because there are a whole host of
    conditions that must be satisfied by the Get message to avoid an
    error (which causes the adapter to squash the read request).
    
    They assertions in the first section also check that the TL interface
    doesn't allow "overlapped" requests.
    
    We then check that write requests are translated properly. This
    includes checking that the we_o, addr_o, be_o and wdata_o signals
    match the message.
    
    We have covers for the two specific write opcodes (to check that e.g.
    we don't only allow PutPartialData).
    
    Next, we have some assertions about the D channel response. Although
    TileLink allows a message sender to drop it at will, we don't want to
    do that here, and so check this is true.
    
    We also check that unknown opcodes get error responses and that error
    responses have d_data == '1.
    
    For responses to Get messages, we check that the data actually match
    the stuff seen on rdata_i.
    
    Finally, we have some basic checks about the register interface. In
    particular, we check the busy_i and error_i signals have the expected
    effects.
    
    Signed-off-by: Rupert Swarbrick <rswarbrick@lowrisc.org>

--- hw/ip/tlul/fpv/vip/tlul_adapter_reg_assert_fpv.sv
+++ hw/ip/tlul/fpv/vip/tlul_adapter_reg_assert_fpv.sv
@@ -20,17 +20,25 @@ module tlul_adapter_reg_assert_fpv #(
   input logic              rst_ni
 );
 
+  // The number of bits needed to address bytes within a data word
+  localparam int SubAW = $clog2(RegDw/8);
+
   default clocking @(posedge clk_i); endclocking
   default disable iff !rst_ni;
 
   // This sequence sees the re_o signal high (which means that we've just decoded a read request on
-  // the TL A channel). It then waits AccessLatency cycles for the response to come back, and a
-  // cycle for it to be registered (in rdata_q).
+  // the TL A channel).
+  sequence read_txn0_S;
+    dut.re_o;
+  endsequence
+
+  // This sequence sees a read request being passed out through re_o and waits a cycle for the
+  // response to come back and be registered in rdata_q.
   //
   // Then we wait an arbitrary number of cycles where the TL D channel is not ready until we finally
   // see the data being accepted.
   sequence read_txn_S;
-    dut.re_o                  ##(AccessLatency + 1)
+    read_txn0_S               ##1
     (!dut.tl_i.d_ready)[*0:$] ##1
     dut.tl_i.d_ready;
   endsequence
@@ -67,4 +75,213 @@ module tlul_adapter_reg_assert_fpv #(
     IntgError_C: cover property (dut.intg_error_o);
   end
 
+  // Translation from A channel message to read request ////////////////////////////////////////////
+
+  // The read or write address requested through the TL A channel gets propagated correctly to the
+  // addr_o port.
+  AChanAddr_A: assert property (dut.re_o || dut.we_o |->
+                                dut.addr_o == {dut.tl_i.a_address[RegAw-1:2], 2'b0});
+
+  // A message is considered reasonable as long as the following properties are all true:
+  //
+  //  - The a_size field is a known value (0, 1 or 2, giving operations on 1, 2 or 4 bytes).
+  //
+  //  - The a_address field is aligned as implied by a_size. (If a_size is N, then the operation
+  //    addresses 2^N bytes and should be naturally aligned, so the bottom 2^N bits of the address
+  //    should be zero).
+  //
+  //  - The a_mask field is only true for active lanes. The sub-word part of the address gives the
+  //    index of the bottom active lane. If a_size is N then there are 2^N active lanes, starting at
+  //    that index.
+  //
+  //  - Either en_ifetch_i is true or the instr_type field is not MuBi4True.
+  //
+  //  - There is no command integrity error (which would be reflected in the intg_error_o on the
+  //    next cycle)
+
+  logic fpv_known_size;
+  assign fpv_known_size = dut.tl_i.a_size <= 2;
+
+  // The number of lanes addressed by an operation with the given a_size
+  int unsigned lane_count;
+  assign lane_count = 1 << dut.tl_i.a_size;
+
+  // The mask of of bits that should be zero at the bottom of the address for it to be correctly
+  // aligned. For example, if there are 4 lanes, then the address should be a multiple of 4 and the
+  // bottom two bits of the address should be zero.
+  logic [RegAw-1:0] bottom_addr_bit_mask;
+  assign bottom_addr_bit_mask = RegAw'(lane_count - 1);
+
+  // True if a_address is correctly aligned for a_size
+  logic fpv_address_aligned;
+  assign fpv_address_aligned = ~|(dut.tl_i.a_address & bottom_addr_bit_mask);
+
+  // The address, modulo the width of a data word.
+  logic [SubAW-1:0] subword_address;
+  assign subword_address = dut.tl_i.a_address[SubAW-1:0];
+
+  // A mask that gives all the active lanes (implied by a_size and a_address)
+  bit [RegBw-1:0] active_lane_mask;
+  assign active_lane_mask = RegBw'((1 << lane_count) - 1) << subword_address;
+
+  // True if a bit in the mask is false for every inactive lane.
+  logic mask_0_in_inactive_lanes;
+  assign mask_0_in_inactive_lanes = ~|(dut.tl_i.a_mask & ~active_lane_mask);
+
+  // True if the instr_type is a valid mubi
+  logic is_valid_instr_type;
+  assign is_valid_instr_type = dut.tl_i.a_user.instr_type inside {prim_mubi_pkg::MuBi4True,
+                                                                  prim_mubi_pkg::MuBi4False};
+
+  // True if fetches are currently allowed
+  logic fetches_allowed;
+  assign fetches_allowed = dut.en_ifetch_i == prim_mubi_pkg::MuBi4True;
+
+  // True if the current operation is allowed with this instr_type. The instr_type must be a valid
+  // mubi value and if it's true then fetches must be allowed and the opcode must be Get.
+  logic valid_with_instr_type;
+  assign valid_with_instr_type = is_valid_instr_type &&
+                                 (dut.tl_i.a_user.instr_type == prim_mubi_pkg::MuBi4True ->
+                                  dut.tl_i.a_opcode == tlul_pkg::Get && fetches_allowed);
+
+  // True if there is a command integrity error on this cycle. This is false unless CmdIntgCheck is
+  // true.
+  //
+  // TODO: We currently cheat and use an internal signal (instead of doing the integrity computation
+  //       in the TB)
+  logic fpv_cmd_intg_err;
+  assign fpv_cmd_intg_err = dut.intg_error;
+
+  // True if the data fields on the A channel are seem reasonable for an operation. Specific
+  // operations (Get, PutPartialData and PutFullData) will need extra checks.
+  logic fpv_reasonable_a_msg_fields;
+  assign fpv_reasonable_a_msg_fields = fpv_known_size && fpv_address_aligned &&
+                                       mask_0_in_inactive_lanes && valid_with_instr_type &&
+                                       !fpv_cmd_intg_err;
+
+  // True if we can see a valid A-channel message with a Get operation.
+  logic fpv_valid_get_message;
+  assign fpv_valid_get_message = dut.tl_i.a_opcode == tlul_pkg::Get &&
+                                 fpv_reasonable_a_msg_fields;
+
+  // True if we are accepting an A-channel message
+  logic fpv_accepted_a_chan_message;
+  assign fpv_accepted_a_chan_message = dut.tl_i.a_valid && dut.tl_o.a_ready;
+
+  // True if the current A-channel message has a known opcode (Get, PutFullData or PutPartialData)
+  logic fpv_known_opcode;
+  assign fpv_known_opcode = dut.tl_i.a_opcode inside {tlul_pkg::Get,
+                                                      tlul_pkg::PutFullData,
+                                                      tlul_pkg::PutPartialData};
+
+  // If a Get message arrives on the A channel and satisifies the properties for a valid A-channel
+  // message, then the re_o signal should go high that cycle.
+  //
+  // We also want to check that we don't see any spurious requests through re_o, so use iff. Note
+  // that ReadReq_C gives a cover for this happening.
+  GetToRe_A: assert property (dut.re_o <->
+                              (fpv_accepted_a_chan_message &&
+                               fpv_valid_get_message));
+
+  // The adapter doesn't allow "overlapped requests", so an accepted message on the A channel means
+  // that a_ready will not be high again until the response has been accepted on the D channel.
+  NoOverlaps_A: assert property (fpv_accepted_a_chan_message |=>
+                                 (!dut.tl_o.a_ready until_with
+                                  dut.tl_o.d_valid && dut.tl_i.d_ready));
+
+  // Translation from A channel message to write request ///////////////////////////////////////////
+
+  // The same checks as fpv_reasonable_a_msg_fields, but also requiring that the address is
+  // word-aligned.
+  logic fpv_reasonable_write_msg_fields;
+  assign fpv_reasonable_write_msg_fields = fpv_reasonable_a_msg_fields &&
+                                           !|dut.tl_i.a_address[1:0];
+
+  // Checks that the fields are reasonable for a PutFullData message
+  logic fpv_valid_pfd_message;
+  assign fpv_valid_pfd_message = dut.tl_i.a_opcode == tlul_pkg::PutFullData &&
+                                 !|(active_lane_mask & ~dut.tl_i.a_mask) &&
+                                 fpv_reasonable_write_msg_fields;
+
+  // Checks that the fields are reasonable for a PutPartialData message (this is weaker than the
+  // PutFullData version, because PutPartialData allows an arbitrary mask)
+  logic fpv_valid_ppd_message;
+  assign fpv_valid_ppd_message = dut.tl_i.a_opcode == tlul_pkg::PutPartialData &&
+                                 fpv_reasonable_write_msg_fields;
+
+  // If a reasonable PutFullData or PutPartialData message arrives on the A channel, the we_o signal
+  // should go high that cycle. This is the only reason for we_o to be high.
+  //
+  // Note that WriteReq_C gives a cover for some write request.
+  PutToWe_A: assert property (dut.we_o <->
+                              (fpv_accepted_a_chan_message &&
+                               (fpv_valid_pfd_message || fpv_valid_ppd_message)));
+
+  // Explicit cover from the PutFullData and PutPartialData write requests triggering we_o (not
+  // implied by WriteReq_C because there are two possible put messages).
+  WriteFromPfd_C: cover property (dut.we_o && dut.tl_i.a_opcode == tlul_pkg::PutFullData);
+  WriteFromPpd_C: cover property (dut.we_o && dut.tl_i.a_opcode == tlul_pkg::PutPartialData);
+
+  // If we_o is high, the value of wdata_o should match the A_DATA field of the A-channel message
+  // that triggers it. This check could theoretically be qualified by be_o, but the stronger
+  // property is true (and easier to state).
+  WDataFromWrite_A: assert property (dut.we_o |->
+                                     dut.wdata_o == dut.tl_i.a_data);
+
+  // If we_o is high, the be_o signal should match the A_MASK field of the A-channel message that
+  // triggers it.
+  ByteEnableFromWrite_A: assert property (dut.we_o |->
+                                          dut.be_o == dut.tl_i.a_mask);
+
+  // Responses on D channel ////////////////////////////////////////////////////////////////////////
+
+  // The adapter never drops a message on the D channel, so d_valid will not drop unless d_ready is
+  // high.
+  DValidStable_A: assert property (##1 $fell(dut.tl_o.d_valid) -> $past(dut.tl_i.d_ready));
+
+  // Similarly, d_data should be constant if d_valid is high unless d_ready has been high to receive
+  // the message.
+  DDataStable_A: assert property (dut.tl_o.d_valid |=>
+                                  $stable(dut.tl_o.d_valid) || $past(dut.tl_i.d_ready));
+
+  // If the adapter has seen any request on the A channel, it will respond by tl_o.d_valid going
+  // high in exactly one cycle (regardless of AccessLatency).
+  DValidWait_A: assert property (fpv_accepted_a_chan_message |=> $rose(dut.tl_o.d_valid));
+
+  // If the adapter sees a request with a bogus opcode, the response on the following cycle with
+  // have D_ERROR set.
+  DNonsenseGetsError_A: assert property (fpv_accepted_a_chan_message && !fpv_known_opcode |=>
+                                         dut.tl_o.d_error);
+
+  // An error response (with D_ERROR true) will always be accompanied by '1 for D_DATA
+  DDataOnesIfError_A: assert property (dut.tl_o.d_error |-> &dut.tl_o.d_data);
+
+  logic [RegDw-1:0] fpv_sampled_rdata;
+  SampleStable_M: assume property (##1 $stable(fpv_sampled_rdata));
+
+  // If re_o is high (so the adapter has just decoded a Get message on the A channel), it will read
+  // and sample rdata_i on this cycle or the next one (depending on AccessLatency). This value will
+  // be returned as D_DATA in the response unless there has been an error, in which case D_ERROR
+  // will be true.
+  RDataToDData_A: assert property (dut.re_o ##AccessLatency dut.rdata_i == fpv_sampled_rdata |->
+                                   ##(1 - AccessLatency)
+                                   (dut.tl_o.d_data == fpv_sampled_rdata) || dut.tl_o.d_error);
+
+  // Properties about the register interface ///////////////////////////////////////////////////////
+
+  // The adapter never requests a read and a write at the same time
+  ReadOrWrite_A: assert property (!(dut.re_o && dut.we_o));
+
+  // If the register interface claims to be busy, the adapter will not accept an A-channel message.
+  NoMsgWhenBusy_A: assert property (dut.busy_i |-> !fpv_accepted_a_chan_message);
+
+  // Error responses from the register interface (through error_i) should be reflected in the
+  // D-channel message.
+  ErrorPropagation_A: assert property ((dut.re_o || dut.we_o) ##AccessLatency dut.error_i |->
+                                       ##(1 - AccessLatency)
+                                       dut.tl_o.d_error);
+
+  // Because the adapter doesn't allow the request and response to overlap, it's not actually
+  // possible for re_o or we_o to be high for multiple consecutive cycles.
+  AccessesPulse_A: assert property (not (dut.re_o || dut.we_o)[*2]);
 endmodule
